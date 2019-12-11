484ccadf8f7689b7cc3928b330c74003809ca835
orris10 <orris10@gmail.com>
2019-12-11 20:45:25 +0000

    day12

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -3,4 +3,7 @@
 set -euxo pipefail
 
 docker build game_api -t orria16/hgop:dev
-GIT_COMMIT=dev docker-compose up
+(cd game_client && npm run build)
+docker build game_client -t orria16/game_client:dev
+
+API_URL=localhost GIT_COMMIT=dev docker-compose up
