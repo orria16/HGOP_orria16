662145ee2d65114ae53c4f4e712ee4918ee18330
orris10 <orris10@gmail.com>
2019-12-11 14:30:53 +0000

    day11

--- scripts/api_test.sh
+++ scripts/api_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/lib/jenkins/terraform/hgop/apitest
-API_URL=http://$(terraform output public_ip):3000
+API_URL=http://$(cd /var/lib/jenkins/terraform/hgop/apitest && terraform output public_ip):3000
 cd -
 API_URL=$API_URL npm run test:api
