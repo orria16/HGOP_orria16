d5f1de49a5fa3796e88f88cebe3d459cf3a36c05
orris10 <orris10@gmail.com>
2019-12-11 16:13:37 +0000

    day11

--- game_api/dealer.unit-test.js
+++ game_api/dealer.unit-test.js
@@ -1,102 +1,26 @@
-const newDealer = require('./dealer.js');
-
-const newRandom = (randomReturnValue) => {
+function newRandom(randomReturnValues) {
+  let i = 0;
   return {
     randomInt: (min, max) => {
-      return randomReturnValue;
+      return randomReturnValues[i++];
     },
   };
-};
+}
 
-// reset dependencies to normal in new tests
-const resetDependencies = () => {
-  return {
-    'random': newRandom(0),
+test('dealer should shuffle cards', () => {
+  // Arrange
+  const dependencies = {
+    'random': () => newRandom([2, 1]),
   };
-};
+  const newDealer = require('./dealer.js');
+  const dealer = newDealer((name) => {
+    return dependencies[name];
+  });
+  const deck = ['a', 'b', 'c'];
 
-// Set starting dependencies
-let dependencies = resetDependencies();
+  // Act
+  dealer.shuffle(deck);
 
-// set context to work like context
-const context = (name) => {
-  return dependencies[name];
-};
-
-describe('Dealer test -shuffle', () => {
-  test('dealer should shuffle cards, deck of 3 and random value 0', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    dependencies.random = newRandom(0);
-    // Arrange
-    const dealer = newDealer(context);
-    const deck = ['a', 'b', 'c'];
-    // Act
-    dealer.shuffle(deck);
-    // Assert
-    expect(deck).toEqual(['b', 'a', 'c']);
-  });
-  test('dealer should shuffle cards, deck of 5 and random value 0', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    dependencies.random = newRandom(0);
-    // Arrange
-    const dealer = newDealer(context);
-    const deck = ['a', 'b', 'c', 'd', 'e'];
-    // Act
-    dealer.shuffle(deck);
-    // Assert
-    expect(deck).toEqual(['d', 'a', 'b', 'c', 'e']);
-  });
-  test('dealer should shuffle cards, deck of 3 and random value 2', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    dependencies.random = newRandom(2);
-    // Arrange
-    const dealer = newDealer(context);
-    const deck = ['a', 'b', 'c'];
-    // Act
-    dealer.shuffle(deck);
-    // Assert
-    expect(deck).toEqual(['c', 'a', 'b']);
-  });
-  test('dealer should shuffle cards and get larger (an undefined), deck of 3 and random value 3', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    dependencies.random = newRandom(3);
-    // Arrange
-    const dealer = newDealer(context);
-    const deck = ['a', 'b', 'c'];
-    // Act
-    dealer.shuffle(deck);
-    // Assert
-    expect(deck.length).toEqual(4);
-  });
-});
-
-describe('dealer test draw', () => {
-  test('dealer should draw one card', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    // Arrange
-    const dealer = newDealer(context);
-    const deck =['a', 'b', 'c'];
-    // Act
-    const cardDrew = dealer.draw(deck);
-    expect(cardDrew).toEqual('c');
-    expect(deck).toEqual(['a', 'b']);
-    expect(deck.length).toEqual(2);
-  });
-  test('dealer should draw two cards', () => {
-    // Set dependencies
-    dependencies = resetDependencies();
-    // Arrange
-    const dealer = newDealer(context);
-    const deck =['a', 'b', 'c'];
-    // Act
-    expect(dealer.draw(deck)).toEqual('c');
-    expect(dealer.draw(deck)).toEqual('b');
-    expect(deck).toEqual(['a']);
-    expect(deck.length).toEqual(1);
-  });
+  // Assert
+  expect(deck).toEqual(['c', 'b', 'a']);
 });
--- game_api/deck.unit-test.js
+++ game_api/deck.unit-test.js
@@ -1,8 +1,26 @@
-const deckConstructor = require('./deck.js');
+function newRandom(randomReturnValues) {
+  let i = 0;
+  return {
+    randomInt: (min, max) => {
+      return randomReturnValues[i++];
+    },
+  };
+}
 
-describe('Testing the deck functionality', () => {
-  test('a deck should have 52 cards', () => {
-    const deck = deckConstructor();
-    expect(deck.length).toEqual(52);
+test('dealer should shuffle cards', () => {
+  // Arrange
+  const dependencies = {
+    'random': () => newRandom([2, 1]),
+  };
+  const newDealer = require('./dealer.js');
+  const dealer = newDealer((name) => {
+    return dependencies[name];
   });
+  const deck = ['a', 'b', 'c'];
+
+  // Act
+  dealer.shuffle(deck);
+
+  // Assert
+  expect(deck).toEqual(['c', 'b', 'a']);
 });
